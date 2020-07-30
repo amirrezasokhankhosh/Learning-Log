672fbd2ad82f156cb51af785305d9195c910dbfd
Amirreza Sokhankhosh <amirrezasokhankhosh79@gmail.com>
2020-07-30 10:36:44 +0430

    end of chapter 18

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # Home page
     url(r'^$', views.index, name='index'),
     url(r'^topics/$' , views.topics , name='topics'),
+    url(r'^topics(?P<topic_id>\d+)/$' , views.topic , name='topic'),
 ]
