da91b5438368d95f56365ba3635420a9de60b5d8
Amirreza Sokhankhosh <amirrezasokhankhosh79@gmail.com>
2020-07-31 11:19:39 +0430

    end of chapter 19

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -20,5 +20,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'', include(('learning_logs.urls' , 'learning_logs'), namespace='learning_logs')),
-    url(r'', include(('users.urls' , 'users') , namespace='users')),
+    url(r'^users/', include(('users.urls' , 'users') , namespace='users')),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, Http404
 
 from .models import Topic , Entry
 from .forms import TopicForm , EntryForm
@@ -9,17 +11,22 @@ def index(request):
     """The home page for Learning Log"""
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics':topics}
     return render(request , 'learning_logs/topics.html' , context)
 
+@login_required
 def topic(request , topic_id):
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic , 'entries':entries}
     return render(request , 'learning_logs/topic.html' , context)
 
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         # NO DATA SUBMITTED
@@ -27,12 +34,15 @@ def new_topic(request):
     else:
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))
 
     content = {'form': form}
     return render(request , 'learning_logs/new_topic.html' , content)
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
@@ -52,11 +62,14 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
 
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
